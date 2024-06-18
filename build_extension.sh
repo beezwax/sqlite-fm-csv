e24935ff6f2701a9bc48790aa515d43f76bef0c4
Simon Brown <simon_b@beezwax.net>
2024-06-18 16:52:44 -0700

    partial fixes for Linux

--- build_extension.sh
+++ build_extension.sh
@@ -3,30 +3,31 @@
 #	HISTORY
 #	2024-06-17 simon_b: created from working script
 
+set -e
 sqlitePath = "~/sqlite"
 csvPath = "~/csv_fm"
 
 
 
-#
-# Need to pull in a copy of SQLite for headers?
-#
-
-if false; then
-	mkdir -p "$sqlitePath"
-	pushd "$sqlitePath"
-	wget https://www.sqlite.org/src/tarball/sqlite.tar.gz
-	tar xzf sqlite.tar.gz
-	mkdir -p bld
-	cd bld
-	../sqlite/configure
-	make
-	popd
-fi
-
-#TODO: Use -DSQLITE_CORE in main sqlite build so we can staticly link in extension?
+#TODO: Use -DSQLITE_CORE in main sqlite build so we can staticly link in extension to sqlite binary?
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
+
+	#
+	# Need to pull in a copy of SQLite for headers?
+	#
+
+	if false; then
+		mkdir -p "$sqlitePath"
+		pushd "$sqlitePath"
+		wget https://www.sqlite.org/src/tarball/sqlite.tar.gz
+		tar xzf sqlite.tar.gz
+		mkdir -p bld
+		cd bld
+		../sqlite/configure
+		make
+		popd
+	fi
 
 	# Typically macOS SDKs are at either of these paths (version numbers may be different)
 	
@@ -39,10 +40,14 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	#install_name_tool -change /usr/lib/libsqlite3.dylib @loader_path./bBox fm_csv.dylib
 
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
 	# Linux
 	#TODO: fix for Ubuntu
+	# sudo apt install libsqlite3-dev
+
 	includesPath="/usr/include"
 	clang -g -I../../Support/sqlite/bld -I"$includes" -fPIC -dynamiclib -L../../Support/sqlite/bld -lsqlite3 fm_csv.c -o fm_csv.dylib
+
 fi
 
 
